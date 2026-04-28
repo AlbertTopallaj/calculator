eb04c66b82537b248c8fb1677beb6e9f866511a1
LeoGamerMan <mirzajanzadeh.leo@gmail.com>
2026-04-28 19:02:32 +0200

    Basic start

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 // Hämtar display-elementet från HTML
 const displayEl = document.getElementById('display');
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('nav');
 
 // Uppdaterar texten i displayen
 function setDisplayValue(value) {
   displayEl.textContent = value;
 }
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('nav');
-
 
 burger.addEventListener('click', () => {
     nav.classList.toggle('open');
@@ -21,4 +20,36 @@ function createKey(symbol, action) {
   return key;
 }
 
-document.body.append(createKey('=',null));
+function createFreeCalculator() {
+  const calculator = document.createElement('div');
+  calculator.id = 'freeCalculator';
+  calculator.className = 'calculator';
+
+  let display = document.createElement('div');
+  display.id = 'display'; 
+  display.className = 'calculator-display';
+  calculator.appendChild(display);
+
+  let keypad = document.createElement('div');
+  keypad.id = 'keypad';
+  keypad.appendChild(createKey('1', null));
+  keypad.appendChild(createKey('2', null));
+  keypad.appendChild(createKey('3', null));
+  keypad.appendChild(createKey('DEL', null));
+  keypad.appendChild(createKey('4', null));
+  keypad.appendChild(createKey('5', null));
+  keypad.appendChild(createKey('6', null));
+  keypad.appendChild(createKey('+', null));
+  keypad.appendChild(createKey('7', null));
+  keypad.appendChild(createKey('8', null));
+  keypad.appendChild(createKey('9', null));
+  keypad.appendChild(createKey('-', null));
+  keypad.appendChild(createKey('AC', null));
+  keypad.appendChild(createKey('0', null));
+  keypad.appendChild(createKey('C', null));
+  keypad.appendChild(createKey('=', null));
+  calculator.appendChild(keypad);
+  return calculator;
+}
+
+document.getElementById('calculatorSection').appendChild(createFreeCalculator());
