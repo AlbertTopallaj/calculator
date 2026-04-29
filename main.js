3fc7b882839de04ec586050e7a30a89df92c5756
LeoGamerMan <mirzajanzadeh.leo@gmail.com>
2026-04-29 21:57:28 +0200

    Size responsive and added helper function

--- main.js
+++ main.js
@@ -13,23 +13,28 @@ burger.addEventListener('click', () => {
 });
 
 function createKey(symbol, action) {
-  const key = document.createElement('button');
+  let key = document.createElement('button');
   key.append(document.createTextNode(symbol));
   key.onclick = action;
-  key.className = 'key';
+  key.classList.add('key');
   return key;
 }
 
 function createFreeCalculator() {
-  const calculator = document.createElement('div');
+  let calculator = document.createElement('div');
   calculator.id = 'freeCalculator';
-  calculator.className = 'calculator';
+  calculator.classList.add('calculator');
 
   let display = document.createElement('div');
   display.id = 'display'; 
-  display.className = 'calculator-display';
+  display.classList.add('calculator-display');
   calculator.appendChild(display);
 
+  calculator.appendChild(freeCalculatorKeypad());
+  return calculator;
+}
+
+function freeCalculatorKeypad() {
   let keypad = document.createElement('div');
   keypad.id = 'keypad';
   keypad.appendChild(createKey('1', null));
@@ -48,8 +53,7 @@ function createFreeCalculator() {
   keypad.appendChild(createKey('0', null));
   keypad.appendChild(createKey('C', null));
   keypad.appendChild(createKey('=', null));
-  calculator.appendChild(keypad);
-  return calculator;
+  return keypad;
 }
 
 document.getElementById('calculatorSection').appendChild(createFreeCalculator());
