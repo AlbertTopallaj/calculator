48e9592d35e105efca4b7daeb591bb47a2498151
Professor <altopallaj21@gmail.com>
2026-04-30 16:18:01 +0200

    feat: add login/signup modal

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const burger = document.querySelector('.burger');
-const nav = document.querySelector('nav');
-
+const nav = document.querySelector('.header-nav');
 
 burger.addEventListener('click', () => {
     nav.classList.toggle('open');
