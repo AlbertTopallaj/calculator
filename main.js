618af15250ffedb0bde11b10ac882f4084d79985
LeoGamerMan <mirzajanzadeh.leo@gmail.com>
2026-05-05 19:24:22 +0200

    Adding numbers and symbols. Delete. Calculate.

--- main.js
+++ main.js
@@ -1,10 +1,54 @@
 // Hämtar display-elementet från HTML
 const displayEl = document.getElementById('display');
+displayEl.textContent = 0;
 
 // Uppdaterar texten i displayen
 function setDisplayValue(value) {
   displayEl.textContent = value;
 }
+
+function addToDisplayValue(value) {
+  let display = document.getElementById('display');
+  let displayValue = display.textContent;
+  if (!isNaN(value)) {
+    if (displayValue == 0) {
+      display.textContent = '';
+    }
+  } else {
+    if (isNaN(displayValue.at(displayValue.length-1))) {
+      display.textContent = displayValue.slice(0, -1);
+    }
+  }
+  display.textContent += value;
+}
+
+function deleteFromDisplayValue() {
+  let display = document.getElementById('display');
+  let value = display.textContent;
+  if (value.length < 2) {
+    display.textContent = 0;
+    return;
+  }
+  display.textContent = value.slice(0, -1);
+}
+
+function calculate() {
+  let display = document.getElementById('display');
+  let value = display.textContent;
+  for (let i = 1; i < value.length-1; i++) {
+    let char = value.at(i);
+    if (char == '(') {
+      value = value.slice(0, i) + '*' + value.slice(i);
+      i++;
+    }
+    if (char == ')') {
+      value = value.slice(0, i+1) + '*' + value.slice(i+1);
+      i += 2;
+    }
+  }
+  display.textContent = eval(value);
+}
+
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.header-nav');
 
@@ -19,8 +63,6 @@ function createKey(symbol, action) {
   key.className = 'key';
   return key;
 }
-
-document.body.append(createKey('=',null));
 
 const sliderContainer = document.querySelector('.slides-container');
 const dots = document.querySelectorAll('.dot');
