3bc143f20140c50ca8cc15b4e4f123d17de17f8a
MorisFili <moris.fili@gmail.com>
2026-05-07 14:17:30 +0200

    dynamic paywall content

--- state.js
+++ state.js
@@ -1,4 +1,4 @@
-import {checkUserSession, logout, signIn, signUp, confirmPayment} from "./db-script.js"
+import {checkUserSession, logout, signIn, signUp, confirmPayment, fetchPayload} from "./db-script.js"
 
 let appState = {
     user: null,
@@ -149,11 +149,26 @@ async function sessionCheck() {
     renderView()
 }
 
+async function getPayload(index) {
+    if (appState.subscription.isPremium) {
+        await fetchPayload("orangeMan", index)
+    } else {
+        const section = document.getElementById(`dynamicCalculatorSection${index}`)
+        const paywall = document.createElement("h1")
+        paywall.textContent = "Premium content placeholder. \nSign up and Go Pro\n.... or Go Home"
+        paywall.style.whiteSpace = "pre-line";
+        paywall.style.lineHeight = "4.0";
+        paywall.style.textAlign = "center";
+        paywall.style.color = "azure"
+        section.appendChild(paywall)
+    }
+}
+
 window.handleLogin = handleLogin
 window.handleSignup = handleSignup
 window.handleLogout = handleLogout
 window.handlePayment = handlePayment
-window.appState = appState
+window.getPayload = getPayload
 
 sessionCheck()
 
